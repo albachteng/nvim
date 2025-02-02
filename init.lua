7d215438c5a667d3b2b54cdf01978b3bb8d62146
albachteng <albachteng@gmail.com>
2025-02-02 11:38:37 -0600

    add terminal options

--- init.lua
+++ init.lua
@@ -3,12 +3,14 @@ require("config.lazy")
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 4
 vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
 
 -- add errors to quickfix
 vim.keymap.set("n", "<space><space>e", ":lua vim.diagnostic.setqflist() <CR><C-w>k")
--- NTS: will not work without overriding windows "cast" feature bullshit
+-- cycle through the quickfix list
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
 -- source the current file
@@ -21,11 +23,25 @@ vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 -- the most important keymap of all time
 vim.keymap.set("i", "jk", "<C-c>")
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+end)
 
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
 	end,
 })
