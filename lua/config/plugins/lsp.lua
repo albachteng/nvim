8719f2eddde80298e80b6e455d4674eba5677a75
albachteng <albachteng@gmail.com>
2025-03-02 11:02:16 -0600

    force fzf version

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -15,7 +15,15 @@ return {
 			},
 		},
 		config = function()
-			local capabilities = require('blink.cmp').get_lsp_capabilities()
+			local blink = require('blink.cmp')
+			blink.setup({
+				fuzzy = {
+					prebuilt_binaries = {
+						force_version = "latest"
+					}
+				}
+			})
+			local capabilities = blink.get_lsp_capabilities()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.clangd.setup({ capabilities = capabilities })
